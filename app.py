8e8ab0f40d073a8edb1bbd7419e7ecbaf0dd6be9
ezizgeldi <fc.chelsea2013@mail.ru>
2023-01-21 20:32:57 +0300

    Create new DockerProject

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient('mongodb', 27017)
+db = client.tododb
+
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def todo():
+    _items = db.tododb.find()
+    items = [item for item in _items]
+    return render_template('todo.html', items=items)
+
+
+@app.route('/new', methods=['POST'])
+
+def new():
+    item_doc = {
+        'task': request.form['task'],
+        'description': request.form['description']
+    }
+    db.tododb.insert_one(item_doc)
+    return redirect(url_for('todo'))
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
